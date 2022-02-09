d9b3398b26f5d2a932cf81643df6d7971568dc8a
franksfotos <schulze.f@gmail.com>
2022-02-09 17:54:24 +0100

    Update wttm.js

--- wttm.js
+++ wttm.js
@@ -1,67 +1,83 @@
 let moondeck = [
-    [5, "E"],
-    [9,  "E"],
-    [5, "P"],
-    [3, "C"],
-    [11, "P"],
-    [13, "C"],
-    [6, "R"],
-    [10, "C"],
-    [6, "A"],
-    [9, "P"],
-    [7, "P"],
-    [6, "P"],
-    [12, "E"],
-    [10, "E"],
-    [8, "W"],
-    [3, "A"],
-    [6, "E"],
-    [8, "C"],
-    [2, "P"],
-    [7, "R"],
-    [11, "E"],
-    [8, "R"],
-    [5, "R"],
-    [7, "E"],
-    [11, "E"],
-    [9, "W"],
-    [8, "P"],
-    [10, "R"],
-    [9, "R"],
-    [14, "P"],
-    [2, "R"],
-    [7, "W"],
-    [7, "E"],
-    [6, "C"],
-    [4, "C"],
-    [4, "P"],
-    [1, "R"],
-    [12, "A"],
-    [13, "A"],
-    [8, "A"],
-    [12, "P"],
-    [10, "W"],
-    [10, "A"],
-    [11, "R"],
-    [5, "R"],
-    [12, "C"],
-    [9, "P"],
-    [7, "P"],
-    [3, "W"],
-    [10, "P"],
-    [15, "R"],
-    [4, "E"],
-    [9, "E"],
-    [15, "E"],
-    [8, "P"],
-    [8, "R"],
-    [5, "E"],
-    [11, "R"],
-    [13, "W"],
-    [14, "R"],
-    [6, "W"],
     [1, "E"],
+    [1, "R"],
+
+    [1, "X"], // Dummy
+    [1, "X"], // Dummy
+    [1, "X"], // Dummy
+    [1, "X"], // Dummy
+
+    [2, "P"],
+    [2, "R"],
+
+    [3, "A"],
+    [3, "W"],
+
     [4, "A"],
+    [4, "C"],
+    [4, "E"],
+    [4, "P"],
+
+    [5, "E"],
+    [5, "E"],
+    [5, "R"],
+    [5, "R"],
+
+    [6, "A"],
+    [6, "C"],
+    [6, "E"],
+    [6, "P"],
+    [6, "R"],
+    [6, "W"],
+
+    [7, "E"],
+    [7, "E"],
+    [7, "P"],
+    [7, "P"],
+    [7, "R"],
+    [7, "W"],
+
+    [8, "A"],
+    [8, "C"],
+    [8, "P"],
+    [8, "P"],
+    [8, "R"],
+    [8, "R"],
+    [8, "W"],
+
+    [9, "E"],
+    [9, "P"],
+    [9, "P"],
+    [9, "R"],
+    [9, "W"],
+
+    [10, "A"],
+    [10, "C"],
+    [10, "E"],
+    [10, "P"],
+    [10, "R"],
+    [10, "W"],
+
+    [11, "E"],
+    [11, "E"],
+    [11, "R"],
+    [11, "R"],
+
+    [12, "A"],
+    [12, "C"],
+    [12, "E"],
+    [12, "P"],
+
+    [13, "A"],
+    [13, "C"],
+    [13, "W"],
+
+    [14, "P"],
+    [14, "R"],
+
+    [15, "E"],
+    [15, "R"]
+
 ]
 
 class Deck {
@@ -72,20 +88,22 @@ class Deck {
     }
 
     shuffle() {
+        console.log(this)
         const { deck } = this;
         let m = deck.length, i;
-      
+
         while (m) {
-          i = Math.floor(Math.random() * m--);
-      
-          [deck[m], deck[i]] = [deck[i], deck[m]];
+            i = Math.floor(Math.random() * m--);
+
+            [deck[m], deck[i]] = [deck[i], deck[m]];
         }
-        
-        this.drawpile=this.deck
+
+        this.drawpile = this.deck.slice()
         this.discardpile = []
 
         return this;
-      }
+    
+    }
 
     draw() {
         this.discardpile.push(this.drawpile.pop())
@@ -94,49 +112,80 @@ class Deck {
 
 function nextcards() {
     cardsOnPile = thirdCards[0].drawpile.length
+
+    if (cardsOnPile === 21) {
+        console.log( '21 Cards on Pile, Number + NextCard anzeigen')
+        for (i = 0; i < 3; i++) {
+            
+            card = thirdCards[i].drawpile[cardsOnPile - 1]
+            nextCard = thirdCards[i].drawpile[cardsOnPile - 2] === undefined ? [0, 'X'] : thirdCards[i].drawpile[cardsOnPile - 2]
+
+            tag = "t" + (i + 1)
+            document.getElementById(tag).innerHTML = card[0]
+
+            tag = "p" + (i + 1)
+            fn = "./img/" + nextCard[1] + ".png"
+            document.getElementById(tag).src = fn
+            
+            tag = "i" + (i + 1)
+            fn = "./img/X.png"
+            document.getElementById(tag).src = fn
+
+            thirdCards[i].draw()
+        }
+    } else 
+    if (cardsOnPile > 0) {
+        for (i = 0; i < 3; i++) {
+            console.log( thirdCards[i].drawpile.length , 'cards on pile ')
+
+            card = thirdCards[i].drawpile[cardsOnPile - 1]
+            nextCard = thirdCards[i].drawpile[cardsOnPile - 2] === undefined ? [0, 'X'] : thirdCards[i].drawpile[cardsOnPile - 2]
     
-    for (i=0;i<3;i++) {
-        card = thirdCards[i].drawpile[cardsOnPile-1]    
-        nextCard = thirdCards[i].drawpile[cardsOnPile-2] === undefined ? [0, 'X'] : thirdCards[i].drawpile[cardsOnPile-2]
-        thirdCards[i].draw()
-        
-        let tag = "t" + (i+1)
-        document.getElementById(tag).innerHTML = card[0] //+ "<span><img width='30' height='30' src='./img/"+ nextCard[1]+".png' class='preview'></span>"
+            tag = "t" + (i + 1)
+            document.getElementById(tag).innerHTML = card[0]
 
-        tag = "i" + (i+1)
-        fn = "./img/" + card[1] + ".png"
-        document.getElementById(tag).src = fn
+            tag = "i" + (i + 1)
+            fn = "./img/" + card[1] + ".png"
+            document.getElementById(tag).src = fn
 
-        tag = "p" + (i+1)
-        fn = "./img/" + nextCard[1] + ".png"
-        document.getElementById(tag).src = fn
+            tag = "p" + (i + 1)
+            fn = "./img/" + nextCard[1] + ".png"
+            document.getElementById(tag).src = fn
+
+            thirdCards[i].draw()
+
+        }
     }
-}
-    /*
-    card1 = thirdCards[0].drawpile[cardsOnPile-1]
- 
-    console.log(card1)
-    console.log(nextCard1)
-    thirdCards[0].draw()
+    else {
+        console.log("only 1 card left")
+    }
 
-    document.getElementById("t1").innerHTML = card1[0] + "<span><img width='30' height='30' src='./img/"+ nextCard1[1]+".png' class='preview'></span>"
-    //document.getElementById("t2").innerHTML = card2[0] + "<span><img width='30' height='30' src='./img/"+ nc2[1]+".png' class='preview'></span>"
-    //document.getElementById("t3").innerHTML = card3[0] + "<span><img width='30' height='30' src='./img/"+ nc3[1]+".png' class='preview'></span>"
-    
-    fn1 = "../img/" + card1[1] + ".png"
-    //fn2 = "../img/" + card2[1] + ".png"
-    //fn3 = "../img/" + card3[1] + ".png"
-    
-    document.getElementById("i1").src = fn1
-    //document.getElementById("i2").src = fn2
-    //document.getElementById("i3").src = fn3
+
+}
+/*
+card1 = thirdCards[0].drawpile[cardsOnPile-1]
+ 
+console.log(card1)
+console.log(nextCard1)
+thirdCards[0].draw()
+
+document.getElementById("t1").innerHTML = card1[0] + "<span><img width='30' height='30' src='./img/"+ nextCard1[1]+".png' class='preview'></span>"
+//document.getElementById("t2").innerHTML = card2[0] + "<span><img width='30' height='30' src='./img/"+ nc2[1]+".png' class='preview'></span>"
+//document.getElementById("t3").innerHTML = card3[0] + "<span><img width='30' height='30' src='./img/"+ nc3[1]+".png' class='preview'></span>"
+ 
+fn1 = "../img/" + card1[1] + ".png"
+//fn2 = "../img/" + card2[1] + ".png"
+//fn3 = "../img/" + card3[1] + ".png"
+ 
+document.getElementById("i1").src = fn1
+//document.getElementById("i2").src = fn2
+//document.getElementById("i3").src = fn3
 }
 */
 
 function shuffledeck() {
-    for (i=0;i<3;i++) {
+    for (i = 0; i < 3; i++) {
         thirdCards[i].shuffle()
-
     }
 }
 
@@ -144,7 +193,8 @@ function shuffledeck() {
 // Sort Setup Deck
 // moondeck.sort()
 
-let allCards = new Deck( moondeck )
+// console.log(moondeck)
+let allCards = new Deck(moondeck)
 
 // Shuffle all 63 Cards
 allCards.shuffle()
@@ -152,9 +202,11 @@ allCards.shuffle()
 let thirdCards = []
 let idx
 
-for (idx=0; idx<3; idx++) {
-    thirdCards.push( new Deck(allCards.deck.slice(21*idx,21*idx+21)) )
-} 
+for (idx = 0; idx < 3; idx++) {
+    thirdCards.push(new Deck(allCards.deck.slice(21 * idx, 21 * idx + 21)))
+}
+
+nextcards()
 
 
 
